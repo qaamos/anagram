a8246de9233b5078003724e1656f87b1927ccb20
Aamos Riihinen <aamos.riihinen@tuni.fi>
2022-06-05 19:43:34 +0300

    Implement database connectivity for generator logic

--- anagram/logic.py
+++ anagram/logic.py
@@ -1,17 +1,25 @@
 import itertools
+import sqlite3
+import os
 
 def main():
     word = ""
     wordPermutations = []
-    anagrams = {}
-    # in final version dictionary will be an actual dictionary found in database
-    dictionary = ["kissa"]
+    anagrams = []
 
+    con = connectToDb()
     word = input("Enter a word: ")
     wordPermutations = generatePermutations(word)
-    anagrams = findRealWords(wordPermutations, dictionary)
+    anagrams = findWordsFromDict(wordPermutations, con)
     print(anagrams)
+    con.close()
     return 1
+
+def connectToDb():
+    path = os.getcwd()
+    parent = os.path.dirname(path)
+    connection = sqlite3.connect(parent+"/db.sqlite3")
+    return connection
 
 def generatePermutations(word):
     permutations = []
@@ -19,9 +27,13 @@ def generatePermutations(word):
     permutations = [''.join(permutation) for permutation in permutations]
     return permutations
 
-def findRealWords(permutations, dictionary):
-    realWords = {}
-    realWords = set(permutations).intersection(dictionary)
+def findWordsFromDict(permutations, connection):
+    realWords = []
+    cursor = connection.cursor()
+    for permutation in permutations:
+        # SQL query should probably be sanitized
+        for result in cursor.execute("SELECT word FROM dictionary WHERE word LIKE '" + permutation + "'"):
+            realWords.append(result[0])
     return realWords
 
 main()
